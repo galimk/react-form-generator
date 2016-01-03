bd661f5108290fc99f07e17b33bf92db88e9093f
Amir Kaudinov <kaudinovamir@gmail.com>
2016-01-03 13:57:20 -0700

    Updating the footer on the index page and getting rid of white spaces. Changed the login-component size, position, and style of the component to look minimal and proper.

--- src/components/shell/components/login-component.js
+++ src/components/shell/components/login-component.js
@@ -5,29 +5,44 @@ var React = require('react');
 
 var LoginModal = React.createClass({
     render: function () {
+        var modalStyle = {
+            marginLeft: '-175px',
+            marginTop: '-150px',
+            left: '50%',
+            top: '50%',
+            position: 'absolute'
+        };
+        var PswdButtonStyle = {
+            margin: '45px'
+        };
+        var signUpButtonStyle = {
+            marginLeft: '-5px'
+        };
         return (
-            <div className="modal-dialog" id="loginModal">
-                <div className="modal-body">
-                    <form className="center-block">
-                        <div className="form-group">
-                            <input type="text" id="loginInputEmail" className="form-control input-lg"
-                                   placeholder="Email"/>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" id="loginInputPassword" className="form-control input-lg"
-                                   placeholder="Password"/>
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-primary btn-sm btn-block">Log in</button>
-                        </div>
-                    </form>
-                    <div>
-                        <span className="pull-left">
-                                <button className="btn btn-primary btn-sm">Forgot Password</button>
-                        </span>
-                        <span className="pull-right">
-                            <button className="btn btn-success btn-sm">Sign up</button>
-                        </span>
+            <div>
+                <div className="modal-dialog" id="loginModal" style={modalStyle}>
+                    <div className="modal-body col-md-7">
+                        <form className="center-block">
+                            <div className="form-group">
+                                <input type="text" id="loginInputEmail" className="form-control input-sm"
+                                       placeholder="Email"/>
+                            </div>
+                            <div className="form-group">
+                                <input type="text" id="loginInputPassword" className="form-control input-sm"
+                                       placeholder="Password"/>
+                            </div>
+                            <div className="form-group">
+                            <span className="pull-left">
+                                <button className="btn btn-primary btn-sm">Log in</button>
+                            </span>
+                                <span className="-align-center" style={PswdButtonStyle}>
+                                    <button className="btn btn-danger btn-sm">Forgot password</button>
+                                </span>
+                            <span className="pull-right">
+                                <button className="btn btn-warning btn-sm" style={signUpButtonStyle}>Sign up</button>
+                            </span>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
