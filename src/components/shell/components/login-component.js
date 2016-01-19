10e06421b3bf2330c951d1ded4646042322ce440
Amir Kaudinov <kaudinovamir@gmail.com>
2016-01-18 18:15:00 -0700

    working on connecting the login component to the backbone model.

--- src/components/shell/components/login-component.js
+++ src/components/shell/components/login-component.js
@@ -2,8 +2,50 @@
  * Created by amirkaudinov on 12/29/15.
  */
 var React = require('react');
+var _ = require('underscore');
+var LoginModel = require('../models/loginModel');
 
-var LoginModal = React.createClass({
+var LoginComponent = React.createClass({
+    getInitialState: function(){
+        return this.setUpModelState();
+    },
+
+    setUpModelState: function(){
+      var loginModelState = {
+            model:{
+            email:{
+                value: LoginModel.get('email'),
+                error: LoginModel.get('email_error')
+            },
+            password:{
+                value: LoginModel.get('password'),
+                error: LoginModel.get('password_error')
+            }
+        }
+      };
+        return loginModelState;
+    },
+
+    componentDidMount: function(){
+        LoginModel.on('change', this.loginModelChanged(), this)
+    },
+
+    componentWillUnmount: function(){
+        LoginModel.on('change', this.loginModelChanged(), this)
+    },
+
+    loginModelChanged: function(){
+        this.setState(this.setUpModelState())
+    },
+
+    onSubmit: function(e){
+        var setter = {};
+        var errorMessages = LoginModel.preValidate(e.target.name, e.target.value);
+        setter[e.target.name + '_error'] = errorMessages ? errorMessages : undefined;
+        setter[e.target.name] = e.target.value;
+        LoginModel.set(setter);
+    },
+
     render: function () {
         return (
             <div id="loginBox" className="mainPanel">
@@ -21,7 +63,7 @@ var LoginModal = React.createClass({
                                 <input type="password" className="form-control marginTop" placeholder="Password"/>
                             </div>
                             <div className="form-group">
-                                <a className="btn loginButton">Log In</a>
+                                <a className="btn loginButton" onclick={this.onSubmit}>Log In</a>
                                 <span className="pull-right textRight">
                                     <a href="#">Forgot Password?</a>
                                 </span>
@@ -41,4 +83,4 @@ var LoginModal = React.createClass({
     }
 });
 
-module.exports = LoginModal;
+module.exports = LoginComponent;
