98f02b5b450d8cab82b20c7e44c5a613dc68462c
Amir Kaudinov <kaudinovamir@gmail.com>
2016-03-28 16:53:14 -0600

    Working on the login-error component.

--- src/components/shell/components/loginError-component.js
+++ src/components/shell/components/loginError-component.js
@@ -5,26 +5,36 @@ var React = require('react');
 var classnames = require('classnames');
 var _ = require('underscore');
 
-function listContainsErrors(list){
-    return list.some(function(item){return item != null})
+function listContainsErrors(list) {
+    return list.some(function (item) {
+        return item != null
+    })
 }
 
 var LoginErrorComponent = React.createClass({
-    render: function(){
+    render: function () {
         var errorClassNames = classnames({
-            'alert-danger': listContainsErrors(this.props.errorList),
-            'alert-dismissible': listContainsErrors(this.props.errorList),
+            'errorBackground': true,
             'errorTrans': true
         });
 
-        var errors = this.props.errorList.map(function(error, i){
-            return(
-                <h5 key={i}>{error} </h5>
-            )
+        var nonErrorClassNames = classnames({
+            'nonErrorTrans': true,
+            'errorBackground': true
+        });
+
+        var errors = this.props.errorList.map(function (error, i) {
+            if (error != null || error != undefined) {
+                return (
+                    <div className={errorClassNames} key={i}>
+                        <strong className='loginErrorText'>{error}</strong>
+                    </div>
+                )
+            }
         }, this);
 
-        return(
-            <div className={errorClassNames}>
+        return (
+            <div>
                 {errors}
             </div>
         )
